334589fec3b2fd993843b17b21143faac1b4f536
Thane Chaves <tmoonder@gmail.com>
2025-02-15 16:50:19 -0500

    Added Center to App component to clean up UI.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import { NewFicForm } from './components/NewFicForm'
 import { Shelf } from './components/Shelf'
+import { Center } from '@chakra-ui/react'
 
 function App() {
   return (
-    <>
+    <Center>
       <Shelf />
       <NewFicForm />
-    </>
+    </Center>
   )
 }
 
