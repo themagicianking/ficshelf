bf6bdc760b8212dd1102b2a2be0450227188debc
Thane Chaves <tmoonder@gmail.com>
2025-02-14 23:20:04 -0500

    Initial server setup.

---
+++ server/index.js
@@ -0,0 +1,9 @@
+import express from "express"
+const APP = express();
+import "dotenv/config";
+APP.use(express.json());
+
+const port = process.env.PORT || 3000;
+APP.listen(port, () => {
+  console.log(`API server listening on port ${port}`);
+});
