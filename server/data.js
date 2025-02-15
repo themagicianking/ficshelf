684745a25e4a00c0082369ccbe9199a7321c6286
Thane Chaves <tmoonder@gmail.com>
2025-02-15 00:03:04 -0500

    Added dummy data file with basic data.

---
+++ server/data.js
@@ -0,0 +1,41 @@
+const FIC1 = {
+  id: 0,
+  title: 'A Court of Tea and Deceit',
+  author: 'wendigo619',
+  fandom: 'Alice (TV 2009)',
+  link: 'https://archiveofourown.org/works/61843738'
+}
+
+const FIC2 = {
+  id: 1,
+  title: 'We Were Good, We Were Gold',
+  author: 'calico_sky',
+  fandom: 'Star Wars',
+  link: 'https://archiveofourown.org/works/63087898'
+}
+
+const FIC3 = {
+  id: 2,
+  title: 'Dream Demon',
+  author: 'Al3xand3r',
+  fandom: 'Supernatural (TV 2005)',
+  link: 'https://archiveofourown.org/works/63089866'
+}
+
+const FIC4 = {
+  id: 3,
+  title: "Time's like a Bomb",
+  author: 'F_Sensei',
+  fandom: 'Arcane: League of Legends (Cartoon 2021)',
+  link: 'https://archiveofourown.org/works/61861171'
+}
+
+const FIC5 = {
+  id: 4,
+  title: 'See You later, Monty Gator',
+  author: 'ClownMcGown',
+  fandom: "Five Nights at Freddy's",
+  link: 'https://archiveofourown.org/works/63090010'
+}
+
+export const SHELF = [FIC1, FIC2, FIC3, FIC4, FIC5]
